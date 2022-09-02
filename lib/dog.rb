7cfa9ab61704a745785c9c4a03d8374ea9a16192
Emma Cohanim <emmacohanim@gmail.com>
2022-09-02 12:57:18 -0700

    commit tree 863a3510f5dd23ddc1db884babe839bb4cc1f4b7

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
+
 class Dog
 
+    # GETTER
+    def name=(dog_name) # takes in argument of dog_name and assigns value to name variable
+        @this_dogs_name = dog_name # uses instance variable
+    end
+
+    # SETTER
+    def name # reports name
+        @this_dogs_name
+    end
 end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+
+puts lassie.name
